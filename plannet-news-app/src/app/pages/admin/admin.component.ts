967fb85929aacb40e4145ad341cec3518dbfc096
Roberto Carettoni <roberto.carettoni@reti.it>
2021-07-03 14:52:37 +0200

    validazione

--- plannet-news-app/src/app/pages/admin/admin.component.ts
+++ plannet-news-app/src/app/pages/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NEWS } from 'src/app/assets/news-data';
 import { News } from 'src/app/models/news';
 
@@ -20,14 +20,15 @@ export class AdminComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      title: new FormControl(),
-      description: new FormControl()
-    });
+      title: new FormControl("", [Validators.required, Validators.maxLength(10)]),
+      description: new FormControl("", [Validators.required])
+    });    
   }
   
   onSubmit() {
     this.news.unshift(this.form.value as News);
     alert('Inserita news ' + this.form.get("title")?.value);
+    this.form.reset();
   }
 
 }
