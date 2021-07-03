1551afb2f5613929a232e4b4452334fd801b836c
Roberto Carettoni <roberto.carettoni@reti.it>
2021-07-03 14:44:52 +0200

    forms

--- plannet-news-app/src/app/pages/admin/admin.component.ts
+++ plannet-news-app/src/app/pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { NEWS } from 'src/app/assets/news-data';
 import { News } from 'src/app/models/news';
 
@@ -12,11 +13,21 @@ export class AdminComponent implements OnInit {
   @Input() title: string;
   @Output() titleChange = new EventEmitter<string>();
 
-  news: News = NEWS[0];
+  news: News[] = NEWS;
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(),
+      description: new FormControl()
+    });
+  }
+  
+  onSubmit() {
+    this.news.unshift(this.form.value as News);
+    alert('Inserita news ' + this.form.get("title")?.value);
   }
 
 }
